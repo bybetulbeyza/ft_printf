bf4dc83c406e95d944a17ab0f65c6c11a644b5e0
Betul Beyza Demirel <betdemir@k1m03s08.42istanbul.com.tr>
2026-09-13 16:55:44 +0300

    first commit

---
+++ ft_printf.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: betdemir@student.42istanbul.com.tr         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 15:25:13 by betdemir          #+#    #+#             */
+/*   Updated: 2026/09/13 16:51:52 by betdemir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int  ft_handle_format (char c, va_list args)
+{
+	int	count;
+
+	count = 0;
+	if (c == 'c')
+		count += ft_print_char(va_arg(args, int));
+	else if (c == 's')
+		count += ft_print_str(va_arg(args, char *));
+	else if (c == 'p')
+		count += ft_print_ptr(va_arg(args, void *));
+	else if (c == 'd' || c == 'i')
+		count += ft_print_nbr(va_arg(args, int));
+	else if (c = 'u')
+		count += ft_print_unsig(va_arg(args, unsigned int));
+	else if (c == 'x' || c == 'X')
+		count += ft_print_hex(va_arg(args, int), c);
+	else if (c == '%')
+		count += ft_print_percent(va_arg(args, int));
+	return (count);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	int	i;
+	int count;
+
+	i = 0;
+	count = 0;
+	va_start(args, format);
+	while (format && format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			count += ft_handle_format(format[i],args);
+		}
+		else
+			count += write(1, &format[i], 1);
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
